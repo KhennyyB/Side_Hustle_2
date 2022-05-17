e7822314eab9d070d16e237b13bef5c9fc528480
Okoli Ekene <khennyphresh@gmail.com>
2022-05-17 19:06:17 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+# Side_Hustle Group_25 Capstone Project 2
+A simple responsive webpage with the following sections only;
+- Navigation bar  (with a hamburger icon for the mobile view)
+- Form that contains the following input fields (username, email address, phone number, password, confirm password, gender, and a submit button) 
+
+
+
+
+## Team Lead
+Ekene Okoli
+## Team Assistant 
+Jimoh Oyindamola 
+## Group Members
+Osondu Chinonso Godwill (Poka)<br>
+Ifeoluwa Aramide<br>
+Hilary Adebowale<br>
+Bello Oyindamola<br>
+Ekenne Kehinde<br>
+Omoniyi Oluwatosin<br>
+Mayowa Akintoye<br>
+Chidebere Daniel<br>
+Mgborogwu Godswill (Mujhiz)<br>
+UD<br>
+Onamade Seunbabara<br>
+Lucky<br>
+Emm Law<br>
+
+# Style Guide
+## Project Instructions
+- Ensure you watch the lesson videos for weeks 4, 5, and 6 and attend week 5 and 6 live classes before starting this project.
+- Use bootstrap or any other framework to make the form section responsive.
+- For the navigation bar section, no framework including bootstrap should be used. Use your custom CSS styling to make it responsive.
+- All input fields in the form section must be validated with vanilla javascript only, no validation library must be used.
+- The input field for the password must have an eye icon so that when it's clicked, the user can hide or show the password.
---
+++ app.js
@@ -0,0 +1,119 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item =>{
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+//validation for inputs
+
+let form = document.querySelector(".form");
+let name = document.getElementById("user");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let pass = document.getElementById("password2");
+
+
+//variable for displaying alert message
+let isEmail = false;
+let isUser = false;
+let isPassword = false;
+let isMatch = false;
+
+
+form.addEventListener("submit",()=>{
+	checkEmpty([name,email,pass,password]);
+	checkLength(name,3,10);
+	checkLength(password,8,15);
+	checkMatch(password,pass);
+	checkEmail(email);
+	
+	let message = document.querySelector(".display-pass");
+	if(isEmail && isMatch && isPassword && isUser){
+		message.style.top = "0";
+		setTimeout(()=>{
+			message.style.top ="-10%";
+		},3000);
+	}
+})
+
+//function to valid check email
+fucntion checkEmail(input){
+	let mail = /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	let checking = mail.exec(input.value.trim());
+	if(!checking){
+			showError(input, "Email is not valid");
+		}else{
+			showPass(input);
+			isEmail = true;
+		}
+}
+
+//function to check password match
+function checkMatch(pass1,pass2){
+	if(pass1.value !== pass2.value){
+		showError(pass2, '${grapInput(pass2)} didn't match');
+	}else{
+		isMatch = true;
+	}
+}
+
+//function to check length
+function checkLength(input,min,max){
+	if(input.value.length > max){
+		showError(input, '${grapInput(input)} must be smaller than ${max}.');
+	}else if(input.value.length < min){
+		showError(input, '${grapInput(input)} must be greater than ${min}.');
+	}else{
+		showPass(input);
+		isPassword = true;
+		isUser = true;
+	}
+}
+
+function checkEmpty(input){
+	input.forEach((item) =>{
+		if(item.value.trim() === ""){
+			showError(item, '${grapInput(item)} must be fill');
+		}else{
+			showError(item);
+		}
+	});
+}
+
+//captialize error message
+function grapInput(input){
+	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//funtion to display error message
+function showError(input,msg){
+	let formItem = input.parentElement;
+	formItem.className = "form-item fail";
+	
+	let span = formItem.querySelector("span");
+	span.innerText = msg;
+}
+
+//function to make a showPass
+function showPass(input){
+	let forItem = input.parentElement;
+	formItem.className = "form-item pass";
+}
